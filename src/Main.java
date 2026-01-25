d3698100e32a038d9b296b164773f4444e17e8bb
Vittorione <vittorio.tiozzo.dev@gmail.com>
2026-01-25 20:01:00 +0100

    feat: if else switch learning

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,20 @@
+import java.io.File;
+
+import section3.IfElseSwitch;
+
 public class Main {
-    
+
+    public static void main(String[] args){
+        IfElseSwitch test = new IfElseSwitch();
+        System.out.println(test.max(5, 3, 4));
+
+        System.out.println(test.isNull("trial"));
+
+        System.out.println(IfElseSwitch.getInstance().isNull("text"));
+
+        System.out.println(IfElseSwitch.getInstance().recoverText(1));
+        System.out.println(IfElseSwitch.getInstance().recoverText(2));
+        System.out.println(IfElseSwitch.getInstance().recoverText(3));
+        System.out.println(IfElseSwitch.getInstance().recoverText(4));
+    }
 }
---
+++ src/section3/IfElseSwitch.java
@@ -0,0 +1,54 @@
+package section3;
+
+public class IfElseSwitch{
+    private static IfElseSwitch singleton;
+
+    public static IfElseSwitch getInstance(){
+        if(singleton == null){
+            singleton = new IfElseSwitch();
+        }
+        return singleton;
+    }
+
+    public int max(int n1, int n2, int n3){
+        int max_n = 0;
+
+        if (n1 > n2 && n1 > n3){
+            max_n = n1;
+        } else if(n2 > n1 && n2 > n3){
+            max_n = n2;
+        } else {
+            max_n = n3;
+        }
+
+        return max_n;
+    }
+
+    public boolean isNull(String text){
+        if (text == null){
+            return true;
+        }
+
+        return false;
+    }
+
+    public String recoverText(int cod){
+        String text = null;
+        switch (cod) {
+            case 1:
+                text = "cod 1";
+                break;
+            case 2:
+                text = "cod 2";
+                break;
+            case 3:
+                text = "cod 3";
+                break;
+        
+            default:
+                text = "trial";
+                break;
+        }
+        return text;
+    }
+}
