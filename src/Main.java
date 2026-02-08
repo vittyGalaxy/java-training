3a0074fac977b800add1989506986ab4d63aa91f
Vittorione <vittorio.tiozzo.dev@gmail.com>
2026-02-08 23:37:12 +0100

    throws learning

--- src/Main.java
+++ src/Main.java
@@ -3,21 +3,11 @@ import section9.ExceptionsTest;
 
 public class Main {
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws ExceptionsTest{
         Exceptions e1 = new Exceptions(null);
         Exceptions e2 = new Exceptions("Hello World!");
 
-        e1.printText();
-        e2.printText();
-
-        e1.callPrintText();
-        e2.callPrintText();
-
-        try {
-            e1.execute();
-            e2.execute();
-        } catch (ExceptionsTest e) {
-            e.printStackTrace();
-        }
+        e1.execute();
+        e2.execute();
     }
 }
