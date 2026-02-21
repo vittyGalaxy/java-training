96459299bb4f23884510ef4b497bcb16b8074967
Vittorione <vittorio.tiozzo.dev@gmail.com>
2026-02-21 23:25:12 +0100

    boxing, autoboxing and unboxing learning

--- src/section11/WrapperClass.java
+++ src/section11/WrapperClass.java
@@ -7,5 +7,18 @@ public class WrapperClass {
         Integer b = new Integer(10);
 
         Integer c = new Integer(a);
+
+        // boxing
+        Double d1 = new Double(10.40);
+
+        double d2 = 90.451;
+
+        Double d3 = new Double(d2);
+
+        // autoboxing
+        Double d4 = 123.34;
+
+        // unboxing
+        double d5 = d4;
     }
 }
