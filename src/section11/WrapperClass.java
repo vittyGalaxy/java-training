d563d601cc809213cbe7fe998c5fe400eff2a06a
Vittorione <vittorio.tiozzo.dev@gmail.com>
2026-02-21 23:18:58 +0100

    wrapper learning

---
+++ src/section11/WrapperClass.java
@@ -0,0 +1,11 @@
+package section11;
+
+public class WrapperClass {
+    public void exampleWrapper(){
+        int a = 10;
+
+        Integer b = new Integer(10);
+
+        Integer c = new Integer(a);
+    }
+}
