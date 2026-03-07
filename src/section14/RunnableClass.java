2f758d7bd34571da7379abcd162499cc55e3ac88
Vittorione <vittorio.tiozzo.dev@gmail.com>
2026-03-07 17:21:11 +0100

    multi-threading learning

--- src/section14/RunnableClass.java
+++ src/section14/RunnableClass.java
@@ -4,5 +4,15 @@ public class RunnableClass implements Runnable{
     @Override
     public void run(){
         System.out.println("thread runnable");
+
+        for(int i = 0; i < 10; i++){
+            System.out.println(i);
+        }
+
+        try{
+            Thread.sleep(3000);
+        } catch(InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/section14/ThreadClass.java
+++ src/section14/ThreadClass.java
@@ -3,6 +3,16 @@ package section14;
 public class ThreadClass extends Thread{
     @Override
     public void run(){
-        System.out.println("thread");
+        System.out.println("thread" + getName());
+
+        for(int i = 0; i < 10; i++){
+            System.out.println(i);
+        }
+
+        try{
+            Thread.sleep(3000);
+        } catch(InterruptedException e){
+            e.printStackTrace();
+        }
     }
 }
