de4e31540d95c69f32b90e2148d43faa09b4d3a0
Vittorione <vittorio.tiozzo.dev@gmail.com>
2026-03-08 21:26:36 +0100

    synchronization learning

--- src/Main.java
+++ src/Main.java
@@ -1,103 +1,27 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ForkJoinPool;
-import java.util.concurrent.Future;
-
-import section14.GetSitePage;
-import section14.GetSitePageExecutor;
-import section14.GetSitePageForkJoin;
-import section14.RunnableClass;
-import section14.ThreadClass;
+import section14.ClientClass;
+import section14.ClientNotSyncClass;
 
 public class Main {
-
-    public static void main(String[] args){
-        Main m = new Main();
-
-        try {
-            m.exampleThreadConcurrency();
-        } catch (InterruptedException e){
-            e.printStackTrace();
-        }
-
-        try {
-            m.exampleExecutorCompetition();
-        } catch (InterruptedException | ExecutionException e){
-            e.printStackTrace();
-        }
-
-        m.exampleConcurrencyForkJoin();
-    }
-
-    private void exampleConcurrencyForkJoin(){
-        ForkJoinPool f = new ForkJoinPool();
-        System.out.println(f.invoke(new GetSitePageForkJoin("http://www.getsitepageforkjoin.it")));
-        System.out.println(f.invoke(new GetSitePageForkJoin("http://www.getsitepageforkjoin.it")));
-
-        f.shutdown();
-    }
-
-    private void exampleExecutorCompetition() throws InterruptedException, ExecutionException{
-        List<Callable<String>> sites = new ArrayList<Callable<String>>();
-        sites.add(new GetSitePageExecutor("http://www.getsitepageexecutor.it"));
-        sites.add(new GetSitePageExecutor("http://www.google.it"));
-
-        ExecutorService ex = Executors.newSingleThreadExecutor();
-        List<Future<String>> out = ex.invokeAll(sites);
-
-        for (Future<String> future : out){
-            System.out.println(future.get());
-        }
-
-        ex.shutdown();
-    }
-
-    private void exampleThreadConcurrency() throws InterruptedException{
-        GetSitePage s1 = new GetSitePage("http://www.getsitepage.it");
-        GetSitePage s2 = new GetSitePage("http://www.google.it");
+    public static void main(String[] args) throws InterruptedException {
+        ClientClass c1 = new ClientClass("Vittorio", 20);
+        ClientClass c2 = new ClientClass("Angelo", 50);
 
         // start
-        s1.start();
-        s2.start();
+        c1.start();
+        c2.start();
 
-        // join
-        s1.join();
-        s2.join();
+        c1.join();
+        c2.join();
 
-        System.out.println("output getsitepage");
-        System.out.println(s1.getContent());
-        
-        System.out.println("output google");
-        System.out.println(s2.getContent());
-    }
 
-    private void exampleThread(){
-        ThreadClass tc1 = new ThreadClass();
-        tc1.setName("thread 1");
+        ClientNotSyncClass c3 = new ClientNotSyncClass("Vittorione", 20);
+        ClientNotSyncClass c4 = new ClientNotSyncClass("Abgelone", 50);
 
-        ThreadClass tc2 = new ThreadClass();
-        tc2.setName("thread 2");
+        // start
+        c3.start();
+        c4.start();
 
-        ThreadClass tc3 = new ThreadClass();
-        tc3.setName("thread 3");
-
-        ThreadClass tc4 = new ThreadClass();
-        tc4.setName("thread 4");
-
-        ThreadClass tc5 = new ThreadClass();
-        tc5.setName("thread 5");
-
-        tc1.start();
-        tc2.start();
-        tc3.start();
-        tc4.start();
-        tc5.start();
-
-        Thread t = new Thread(new RunnableClass());
-        t.start();
+        c3.join();
+        c4.join();
     }
 }
