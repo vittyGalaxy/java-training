cab7c6f6de33819236201b7b85fe32d0bf620c79
Vittorione <vittorio.tiozzo.dev@gmail.com>
2026-04-02 18:57:06 +0200

    servlet and httpservlet learning

---
+++ src/section27/DownloadPdf.java
@@ -0,0 +1,23 @@
+package section27;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+
+@WebServlet("/downloadpdf")
+public class DownloadPdf extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    public DownloadPdf() {
+        super();
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.getWriter().append("Served at: ").append(request.getContextPath());
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+}
